9743d42a2b27ecbf24c122512e27e11c1683fde8
scbc-app <lengwetembo@gmail.com>
2026-02-04 20:17:05 +0200

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/juba-application-pro/sw.js', { scope: '/juba-application-pro/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
